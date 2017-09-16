901fc8fb148b669143d4e416fec052b42ef70780
VVen1203 <1803751391@qq.com>
2017-09-16 10:28:26 +0800

    mychange 2

--- words.h
+++ words.h
@@ -1,13 +1,29 @@
 ﻿#ifndef WORDS_H
 #define WORDS_H
+#include<iostream>
 using namespace std;
 
 class words
 {
+string japanese;
+string pronunciation;
+string chinese;
+string property;
 
 public:
     words();
-
+string get_japanese()const{
+    return japanese;
+}
+string get_pronunciation()const{
+    return pronunciation;
+}
+string get_chinese()const{
+    return chinese;
+}
+string get_property()const{
+    return property;
+}
 
 };
 
