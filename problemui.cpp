09e541c52402b6fb2cc68f6501d5040f81ee393b
JUNLIN CHEN <gamefy2010@qq.com>
2017-09-20 23:00:30 +0800

    2017.9.29final

--- problemui.cpp
+++ problemui.cpp
@@ -3,12 +3,19 @@
 ProblemUI::ProblemUI(QWidget *parent) : QWidget(parent)
 {
     this->setGeometry(0,0,parent->width(),parent->height());
+    this->setStyleSheet("QPushButton{border:1px groove #A2E1FF;border-radius:5px;padding:0px 0px;color:#000000;font-size:20px;font-family:Microsoft YaHei;}"
+                        "QPushButton{background:#E2F1FF;}"
+                        "QPushButton:hover{background:#D2E1FF;}"
+                        "QPushButton:pressed{background:#C2E1FF;}"
+                        "QLabel{border:0px groove gray;}");
 
+    Tips=new QLabel(this);
     ProblemDescribe= new QLabel(this);
     ProblemOption1= new QPushButton(this);
     ProblemOption2= new QPushButton(this);
     ProblemOption3= new QPushButton(this);
     ProblemOption4= new QPushButton(this);
+    NextTest=new QPushButton(this);
 
     ProblemDescribe->setText("日本語");
     ProblemOption1->setText("日语");
@@ -19,7 +26,18 @@ ProblemUI::ProblemUI(QWidget *parent) : QWidget(parent)
     ProblemDescribe->move(this->width()*0.15,this->height()*0.18);
     ProblemDescribe->setStyleSheet("font-size:35px;");
     ProblemOption1->setGeometry(this->width()*0.06,this->height()*0.3,560,60);
-    ProblemOption2->setGeometry(this->width()*0.06,this->height()*0.45,560,60);
-    ProblemOption3->setGeometry(this->width()*0.06,this->height()*0.6,560,60);
-    ProblemOption4->setGeometry(this->width()*0.06,this->height()*0.75,560,60);
+    ProblemOption2->setGeometry(this->width()*0.06,this->height()*0.43,560,60);
+    ProblemOption3->setGeometry(this->width()*0.06,this->height()*0.56,560,60);
+    ProblemOption4->setGeometry(this->width()*0.06,this->height()*0.69,560,60);
+
+    NextTest->setText("下一题");
+    NextTest->setGeometry(this->width()*0.65,this->height()*0.83,90,33);
+    NextTest->setStyleSheet("QPushButton{border:1px groove gray;border-radius:5px;padding:0px 0px;color:#ffffff;font-size:15px;}"
+                            "QPushButton{background:#62B1E1;}"
+                            "QPushButton:hover{background:#42A1E1;}"
+                            "QPushButton:pressed{background:#2281B1;}"
+                            "QLabel{border:0px groove gray;}");
+    Tips->setGeometry(this->width()*0.35,this->height()*0.83,200,25);
+    Tips->setText("回答正确，请继续下一题吧~");
+    Tips->setStyleSheet("font-size:15px");
 }
--- problemui.h
+++ problemui.h
@@ -13,11 +13,14 @@ public:
     explicit ProblemUI(QWidget *parent = nullptr);
 
     QLabel *ProblemDescribe;
+    QLabel *Tips;
 
     QPushButton *ProblemOption1;
     QPushButton *ProblemOption2;
     QPushButton *ProblemOption3;
     QPushButton *ProblemOption4;
+
+    QPushButton *NextTest;
 
     management *MainManagement;
 signals:
